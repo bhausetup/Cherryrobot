1ed5e9a84ac190800512cae1b9dfdb2f7aafd057
DOREMON <109501363+bhausetup@users.noreply.github.com>
2023-05-13 10:07:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ if os.path.exists("local.env"):
 load_dotenv()
 class Config(object):
     log = True
-    APP_ID = getenv("API_ID", "6435225")
-    API_HASH = getenv("API_HASH", "4e984ea35f854762dcde906dce426c2d")
+    APP_ID = getenv("API_ID", "26561490")
+    API_HASH = getenv("API_HASH", "1cf95f489c37be14568c94a8d09916b8")
     TOKEN = getenv("TOKEN", "")
     OWNER_ID = getenv("OWNER_ID", "")
     ASSISTANT_ID = getenv("ASSISTANT_ID", "")
@@ -35,10 +35,10 @@ class Config(object):
     BAN_STICKER = getenv("BAN_STICKER", "")
     ALLOW_EXCL = True
     CUSTOM_CMD = False
-    CHANNEL = getenv("CHANNEL", "TheUpdatesChannel")
-    SUPPORT = getenv("SUPPORT", "TheSupportChat")
-    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png")
-    CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
+    CHANNEL = getenv("CHANNEL", "ITSBOTS")
+    SUPPORT = getenv("SUPPORT", "AdulT_R00M")
+    START_IMG = os.environ.get("START_IMG", "https://graph.org/file/7c1dffc198eef66eaf21f.jpg")
+    CMD_IMG = os.environ.get("CMD_IMG", "https://graph.org/file/7c1dffc198eef66eaf21f.jpg")
     CASH_API_KEY = getenv("CASH_API_KEY", "https://www.alphavantage.co/support/#api-key")
     TIME_API_KEY = getenv("TIME_API_KEY", "https://timezonedb.com/api")
     WALL_API = getenv("WALL_API", "https://wall.alphacoders.com/api.php")
@@ -49,5 +49,5 @@ class Config(object):
     BOT_API_URL = getenv("BOT_API_URL", "https://api.telegram.org/bot")
     BOT_API_FILE_URL = getenv("BOT_API_FILE_URL", "https://api.telegram.org/file/bot")
     SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1669178360").split()))
-    ZAID_USER = list(map(int, getenv("DEV_USERS", "1669178360").split()))
+    CHERRY_USER = list(map(int, getenv("DEV_USERS", "1669178360").split()))
     NO_LOAD = list(map(int, getenv("NO_LOAD", "").split()))
